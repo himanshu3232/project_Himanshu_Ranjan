88ec38cad6c603df5412270a3ea24a7f51424efe
himanshu3232 <himanshu2000ranjan@gmail.com>
2024-03-08 22:49:52 +0530

    Completed: React-3 Task-1 and Task-2

--- React-3(redux)/task-1 and task-2/src/components/LowerPart.tsx
+++ React-3(redux)/task-1 and task-2/src/components/LowerPart.tsx
@@ -1,7 +1,10 @@
+import { useSelector } from "react-redux";
+
 export default function LowerPart() {
+  const likes = useSelector((store: any) => store.card.likes);
   return (
     <div>
-      <p>2 likes</p>
+      <p>{likes} likes</p>
       <div className="watch-hrs">
         <p style={{ color: "green" }}>97% Match</p>
         <p>2h 15min</p>
--- React-3(redux)/task-1 and task-2/src/redux/cardSlice.ts
+++ React-3(redux)/task-1 and task-2/src/redux/cardSlice.ts
@@ -1,30 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-interface CardContext {
-  likes: number;
-  descriptionVisibility: boolean;
-}
-const initialState: CardContext = {
-  likes: 0,
+const initialState = {
   descriptionVisibility: false,
+  likes: 0,
 };
 
 const cardSlice = createSlice({
   name: "card",
   initialState,
   reducers: {
-    incrementLikesCount: (state) => {
-      state.likes++;
-    },
-    decrementLikesCount: (state) => {
-      state.likes--;
-    },
     toggleDescription: (state) => {
       state.descriptionVisibility = !state.descriptionVisibility;
+    },
+    incrementLikesCount: (state) => {
+      state.likes += 1;
+    },
+    decrementLikesCount: (state) => {
+      state.likes -= 1;
     },
   },
 });
 
-export const { incrementLikesCount, toggleDescription, decrementLikesCount } =
-  cardSlice.actions;
+export const { toggleDescription, incrementLikesCount, decrementLikesCount } = cardSlice.actions;
 export default cardSlice.reducer;
